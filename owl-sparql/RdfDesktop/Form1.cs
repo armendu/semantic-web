1576376fd16e797c222bcf5794999a9eafd85018
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-06-08 18:17:03 +0200

    Initial commit

---
+++ owl-sparql/RdfDesktop/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Datasets;
+using VDS.RDF.Writing.Formatting;
+
+namespace RdfDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IGraph g = new Graph();
+            FileLoader.Load(g, @"Ontologies/pizza.owl");
+            
+            //First we need an instance of the SparqlQueryParser
+            SparqlQueryParser parser = new SparqlQueryParser();
+
+            //Create a dataset for our queries to operate over
+            //We need to explicitly state our default graph or the unnamed graph is used
+            //Alternatively you can set the second parameter to true to use the union of all graphs
+            //as the default graph
+            InMemoryDataset ds = new InMemoryDataset(g);
+
+            //Then we can parse a SPARQL string into a query
+            SparqlQuery q = parser.ParseFromString(@"
+            PREFIX pizza: <http://www.co-ode.org/ontologies/pizza/pizza.owl#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+
+            SELECT * WHERE {
+              ?topping rdfs:subClassOf pizza:PizzaTopping .
+              ?topping rdfs:subClassOf ?restriction .
+              ?restriction rdf:type owl:Restriction .
+              ?restriction ?p ?o .
+            }");
+
+            //Get the Query processor
+            ISparqlQueryProcessor processor = new LeviathanQueryProcessor(ds);
+
+            var results = processor.ProcessQuery(q);
+            if (results is IGraph resultsGraph)
+            {
+                //Print out the Results
+                NTriplesFormatter formatter = new NTriplesFormatter();
+                foreach (Triple t in resultsGraph.Triples)
+                {
+                    Console.WriteLine(t.ToString(formatter));
+                }
+                Console.WriteLine("Query took " + q.QueryExecutionTime + " milliseconds");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ owl-swrl/main/src/main/java/hello/Application.java
@@ -0,0 +1,76 @@
+package hello;
+
+import org.semanticweb.owlapi.formats.PrefixDocumentFormat;
+import org.semanticweb.owlapi.model.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import java.io.File;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.swrlapi.core.SWRLRuleEngine;
+import org.swrlapi.factory.SWRLAPIFactory;
+import org.swrlapi.parser.SWRLParseException;
+import org.swrlapi.sqwrl.SQWRLQueryEngine;
+import org.swrlapi.sqwrl.SQWRLResult;
+import org.swrlapi.sqwrl.exceptions.SQWRLException;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            try {
+                // Create an OWL ontology using the OWLAPI
+                OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+
+                File file = new File("C:\\Users\\Armend Ukëhaxhaj\\Documents\\Workspace\\Personal\\semantic-web\\owl-sparql\\RdfDesktop\\Ontologies\\pizza.owl");
+
+                OWLOntology ontology = ontologyManager.loadOntologyFromOntologyDocument(file);
+
+                OWLDocumentFormat format = ontologyManager.getOntologyFormat(ontology);
+//                PrefixDocumentFormat prefix = format.asPrefixOWLOntologyFormat();
+//                prefix.setPrefix("pizza", "<http://www.co-ode.org/ontologies/pizza/pizza.owl#>");
+
+                // Create a SWRL rule engine using the SWRLAPI
+                SWRLRuleEngine ruleEngine = SWRLAPIFactory.createSWRLRuleEngine(ontology);
+
+                // Run the rule engine
+                ruleEngine.infer();
+
+                // Create SQWRL query engine using the SWRLAPI
+                SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+                // Create and execute a SQWRL query using the SWRLAPI
+                SQWRLResult result = queryEngine.runSQWRLQuery("q1", "swrlb:add(?x, 2, 2) -> sqwrl:select(?x)");
+
+                // Process the SQWRL result
+                if (result.next())
+                    System.out.println("x: " + result.getLiteral("x").getInteger());
+
+            } catch (OWLOntologyCreationException e) {
+                System.err.println("Error creating OWL ontology: " + e.getMessage());
+                System.exit(-1);
+            } catch (SWRLParseException e) {
+                System.err.println("Error parsing SWRL rule or SQWRL query: " + e.getMessage());
+                System.exit(-1);
+            } catch (SQWRLException e) {
+                System.err.println("Error running SWRL rule or SQWRL query: " + e.getMessage());
+                System.exit(-1);
+            } catch (RuntimeException e) {
+                System.err.println("Error starting application: " + e.getMessage());
+                System.exit(-1);
+            }
+        };
+    }
+
+}
